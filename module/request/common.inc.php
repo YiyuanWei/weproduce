d6c70d4efd56596614f63fd7a1d79fead340d826
y.wei22 <y.wei22@student.unimelb.edu.au>
2020-05-15 22:21:21 +1000

    test

--- module/request/common.inc.php
+++ module/request/common.inc.php
@@ -3,6 +3,7 @@ defined('IN_DESTOON') or exit('Access Denied');
 require DT_ROOT.'/include/module.func.php';
 require DT_ROOT.'/module/'.$module.'/global.func.php';
 $REQ = 'requirement';
+
 $table = $DT_PRE.$module.'_'.$moduleid;
 $table_data = $DT_PRE.$module.'_data_'.$moduleid;
 $table_garment = $DT_PRE.$module.'_garment_'.$moduleid;
